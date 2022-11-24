f202ff7f35a1caa6547fceb59710647eb7ab05cf
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-11-24 14:02:22 +0000

    use shell arguments

--- kpack/setup.kpack.sh
+++ kpack/setup.kpack.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-echo "Enter docker username"
-read dockerUserName
-echo "enter docker password"
-read dockerPassword
-
+echo "shell dockerusername dockerpassword"
 kubectl apply -f https://github.com/pivotal/kpack/releases/download/v0.8.1/release-0.8.1.yaml
 kubectl create secret docker-registry spring-demo-credentials \
-    --docker-username=$dockerUserName \
-    --docker-password=$dockerPassword \
+    --docker-username=$1 \
+    --docker-password=$2 \
     --docker-server=https://index.docker.io/v1/ \
     --namespace default
 kubectl apply -f service-account.yaml
 kubectl apply -f store.yaml
 kubectl apply -f stack.yaml
 kubectl apply -f builder.yaml
-ytt -f image.yaml --data-value dockerusername=$dockerUserName | kubectl apply -f -
+ytt -f image.yaml --data-value dockerusername=$1 | kubectl apply -f -
