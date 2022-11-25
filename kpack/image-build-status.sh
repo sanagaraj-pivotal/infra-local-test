6254c1e4b0e04fb68baf23b55ae3ea524230988e
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-11-25 14:57:55 +0000

    Good state

--- kpack/image-build-status.sh
+++ kpack/image-build-status.sh
@@ -2,18 +2,27 @@
 
 APP_NAME=spring-demo-app
 ReadyStatus=0
-
+FailedStatus=0
 
 ####
 # status
 # status -> Unknown -> True
-while [ $ReadyStatus -le 0 ]
+# status -> False -> Error
+while [ $ReadyStatus -le 0 -a $FailedStatus -le 0 ]
 do
 echo "Image being built, waiting for image status to be ready..."
 ReadyStatus=$(kubectl get images $APP_NAME -o json | jq '.status.conditions[] | select(.type=="Ready") | select(.status=="True")' | wc -c | bc)
-sleep 1
-# TODO: when there is an error handle it
+FailedStatus=$(kubectl get images $APP_NAME -o json | jq '.status.conditions[] | select(.type=="Ready")| select(.status=="False")' | wc -c | bc)
+
+sleep 2
 # TODO: Dont be here forever, have an upper limit with count
 done
 
-echo "Image is ready"
+if [ $FailedStatus -gt 0 ]
+then
+  echo Failed
+  exit 1
+else
+  echo "Image published"
+fi
+
