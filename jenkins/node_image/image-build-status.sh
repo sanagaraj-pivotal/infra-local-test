2d8d0b1396602516f5344ebdf64de36e4d695499
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-11-28 14:31:56 +0000

    Good state

--- jenkins/node_image/image-build-status.sh
+++ jenkins/node_image/image-build-status.sh
@@ -16,9 +16,7 @@ ReadyStatus=$(kubectl get images $APP_NAME -o json | jq '.status.conditions[] | 
 FailedStatus=$(kubectl get images $APP_NAME -o json | jq '.status.conditions[] | select(.type=="Ready")| select(.status=="False")' | wc -c | bc)
 #get pod name
 # inspect container logs
-ExtractedRevision=$(echo "'.items[] | select(.spec.source.git.revision==$GIT_COMMIT) | .metadata.name'")
-echo $ExtractedRevision
-BuilderName=$(kubectl get build -o json | jq -r $ExtractedRevision)
+BuilderName=$(kubectl get build -o json | jq -r '.items[] | select(.spec.source.git.revision=="'$GIT_COMMIT'") | .metadata.name')
 BuilderPodName=$(echo $BuilderName-build-pod)
 sleep 2
 kubectl logs -f $BuilderPodName -c analyze
