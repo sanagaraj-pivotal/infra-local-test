ecb68848758a1613e1b704b744b1112cadc7c35a
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-12-07 11:12:21 +0000

    new way of verifying git changes

--- jenkins/node_image/push-only-if-changes-found.sh
+++ jenkins/node_image/push-only-if-changes-found.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-git status | grep -q modified
 IS_DIFF=$(git diff origin/main main | wc -c | bc)
 if [ $IS_DIFF -eq 0 ]
 then
     echo "No changes since last run"
 else
-    git commit -m "$1"
     git push origin HEAD:main
     echo "New changes"
 fi
