0a612862676db4e53679a27b73c6c6570e0e1a10
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-12-14 13:52:16 +0000

    Adding mailhog

--- jenkins/node_image/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t jenkins-node:latest .
--- jenkins/node_image/push-only-if-changes-found.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-IS_DIFF=$(git diff origin/main main | wc -c | bc)
-if [ $IS_DIFF -eq 0 ]
-then
-    echo "No changes since last run"
-else
-    git push origin HEAD:main
-    echo "New changes"
-fi
